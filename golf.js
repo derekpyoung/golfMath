ba1b2676e5cd3c89d0150b2737433e25edba1048
derekpyoung <derekpyoung12@gmail.com>
2021-06-01 13:01:41 -0700

    working 3-4 players

--- golf.js
+++ golf.js
@@ -7,6 +7,7 @@ var holes = 18;
 
 
 function submit() {
+
   var holeBet = document.getElementById('perHole').value;
   var ldBet = document.getElementById('longestCost').value;
   var kpBet = document.getElementById('closestCost').value;
@@ -31,23 +32,23 @@ function submit() {
   playerThree.kps = document.getElementById('threeKps').value;
 
   var playerFour = {};
-  playerFour.name = document.getElementById('playerFour').value;
-  playerFour.totalWins = document.getElementById('fourWins').value;
-  playerFour.lds = document.getElementById('fourLds').value;
-  playerFour.kps = document.getElementById('fourKps').value;
+  playerFour.name = document.getElementById('playerFour').value ;
+  playerFour.totalWins = document.getElementById('fourWins').value || 0;
+  playerFour.lds = document.getElementById('fourLds').value || 0;
+  playerFour.kps = document.getElementById('fourKps').value || 0;
 
-  var splitsOne = document.getElementById('splitsOne').value
-  var splitsTwo = document.getElementById('splitsTwo').value
-  var splitsThree = document.getElementById('splitsThree').value
-  var splitsFour = document.getElementById('splitsFour').value
+  var splitsOne = document.getElementById('splitsOne').value;
+  var splitsTwo = document.getElementById('splitsTwo').value;
+  var splitsThree = document.getElementById('splitsThree').value;
+  var splitsFour = document.getElementById('splitsFour').value;
 
-  var totalSplits = (parseInt(splitsOne) + parseInt(splitsTwo) + parseInt(splitsThree) + parseInt(splitsFour));
+  var totalSplits = (parseInt(splitsOne) + parseInt(splitsTwo) + parseInt(splitsThree) + parseInt(splitsFour || 0));
   console.log(totalSplits + " splits total");
 
-  var splitOneTot = (parseInt(splitsOne * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsOne)) * holeBet ))) ;
-  var splitTwoTot = (parseInt(splitsTwo * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsTwo)) * holeBet ))) ;
-  var splitThreeTot = (parseInt(splitsThree * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsThree)) * holeBet ))) ;
-  var splitFourTot = (parseInt(splitsFour * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsFour)) * holeBet ))) ;
+  var splitOneTot = (parseInt(splitsOne * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsOne)) * holeBet )|| 0)) ;
+  var splitTwoTot = (parseInt(splitsTwo * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsTwo)) * holeBet )|| 0)) ;
+  var splitThreeTot = (parseInt(splitsThree * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsThree)) * holeBet )|| 0)) ;
+  var splitFourTot = (parseInt(splitsFour * holeBet) - ( ((parseInt(totalSplits / 2) - parseInt(splitsFour)) * holeBet ) || 0));
 
 
 
@@ -59,6 +60,7 @@ function submit() {
 
 
   console.log(holeBet,ldBet,kpBet);
+  console.log(playerNumber);
   console.log(playerOne);
   console.log(playerTwo);
   console.log(playerThree);
@@ -77,95 +79,95 @@ function submit() {
                   console.log(totalLds + " total LDs won");
 
 
-  var kpPayoutOne = parseInt((playerOne.kps) * kpBet) * 3;
-  var kpPayoutTwo = parseInt((playerTwo.kps) * kpBet) * 3;
-  var kpPayoutThree = parseInt((playerThree.kps) * kpBet) * 3;
-  var kpPayoutFour = parseInt((playerFour.kps) * kpBet) * 3;
-  // console.log(kpPayoutOne + playerOne.name + 'kpwin');
-  // console.log(kpPayoutTwo + playerTwo.name + 'kpwin');
-  // console.log(kpPayoutThree + playerThree.name + 'kpwin');
-  // console.log(kpPayoutFour + playerFour.name + 'kpwin');
+  var kpPayoutOne = parseInt((playerOne.kps) * kpBet) * parseInt(playerNumber - 1);
+  var kpPayoutTwo = parseInt((playerTwo.kps) * kpBet) * parseInt(playerNumber - 1);
+  var kpPayoutThree = parseInt((playerThree.kps) * kpBet) * parseInt(playerNumber - 1);
+  var kpPayoutFour = parseInt((playerFour.kps) * kpBet) * parseInt(playerNumber - 1);
+  console.log(kpPayoutOne + playerOne.name + ' kpwin');
+  console.log(kpPayoutTwo + playerTwo.name + ' kpwin');
+  console.log(kpPayoutThree + playerThree.name + ' kpwin');
+  console.log(kpPayoutFour + playerFour.name + ' kpwin');
 
   var playerOneKpTakehome =  parseInt((totalKps) - parseInt(playerOne.kps)) * kpBet;
   var playerTwoKpTakehome = parseInt((totalKps) - parseInt(playerTwo.kps)) * kpBet;
   var playerThreeKpTakehome = parseInt((totalKps) - parseInt(playerThree.kps)) * kpBet;
   var playerFourKpTakehome = parseInt((totalKps) - parseInt(playerFour.kps)) * kpBet;
-  // console.log(playerOneKpTakehome + " kp loss");
-  // console.log(playerTwoKpTakehome + " kp loss");
-  // console.log(playerThreeKpTakehome + " kp loss");
-  // console.log(playerFourKpTakehome + " kp loss");
+  console.log(playerOneKpTakehome + " kp loss");
+  console.log(playerTwoKpTakehome + " kp loss");
+  console.log(playerThreeKpTakehome + " kp loss");
+  console.log(playerFourKpTakehome + " kp loss");
 
   var playerOneKpTotal = parseInt(kpPayoutOne - playerOneKpTakehome);
   var playerTwoKpTotal = parseInt(kpPayoutTwo - playerTwoKpTakehome);
   var playerThreeKpTotal = parseInt(kpPayoutThree - playerThreeKpTakehome);
   var playerFourKpTotal = parseInt(kpPayoutFour - playerFourKpTakehome);
-  // console.log(playerOneKpTotal);
-  // console.log(playerTwoKpTotal);
-  // console.log(playerThreeKpTotal);
-  // console.log(playerFourKpTotal);
+  console.log("PlayerOneKpTotal: " + playerOneKpTotal);
+  console.log("PlayerOneKpTotal: " + playerTwoKpTotal);
+  console.log("PlayerOneKpTotal: " + playerThreeKpTotal);
+  console.log("PlayerOneKpTotal: " + playerFourKpTotal);
 
-  var ldPayoutOne = parseInt((playerOne.lds) * ldBet) * 3;
-  var ldPayoutTwo = parseInt((playerTwo.lds) * ldBet) * 3;
-  var ldPayoutThree = parseInt((playerThree.lds) * ldBet) * 3;
-  var ldPayoutFour = parseInt((playerFour.lds) * ldBet * 3);
-  // console.log(ldPayoutOne + playerOne.name + "LD");
-  // console.log(ldPayoutTwo + playerTwo.name + "LD");
-  // console.log(ldPayoutThree + playerThree.name + "LD");
-  // console.log(ldPayoutFour + playerFour.name + "LD");
+  var ldPayoutOne = parseInt((playerOne.lds) * ldBet) * parseInt(playerNumber - 1);
+  var ldPayoutTwo = parseInt((playerTwo.lds) * ldBet) * parseInt(playerNumber - 1);
+  var ldPayoutThree = parseInt((playerThree.lds) * ldBet) * parseInt(playerNumber - 1);
+  var ldPayoutFour = parseInt((playerFour.lds) * ldBet * parseInt(playerNumber - 1));
+  console.log(ldPayoutOne + playerOne.name + " LD");
+  console.log(ldPayoutTwo + playerTwo.name + " LD");
+  console.log(ldPayoutThree + playerThree.name + " LD");
+  console.log(ldPayoutFour + playerFour.name + " LD");
 
   var playerOneLdLoss = parseInt((totalLds) - parseInt(playerOne.lds)) * ldBet;
   var playerTwoLdLoss = parseInt((totalLds) - parseInt(playerTwo.lds)) * ldBet;
   var playerThreeLdLoss = parseInt((totalLds) - parseInt(playerThree.lds)) * ldBet;
   var playerFourLdLoss = parseInt((totalLds) - parseInt(playerFour.lds)) * ldBet;
-  // console.log(playerOneLdLoss + " ld loss");
-  // console.log(playerTwoLdLoss + " ld loss");
-  // console.log(playerThreeLdLoss + " ld loss");
-  // console.log(playerThreeLdLoss + " ld loss");
+  console.log(playerOneLdLoss + " ld loss");
+  console.log(playerTwoLdLoss + " ld loss");
+  console.log(playerThreeLdLoss + " ld loss");
+  console.log(playerThreeLdLoss + " ld loss");
 
 var ldFinalOne = ldPayoutOne - playerOneLdLoss;
 var ldFinalTwo = ldPayoutTwo - playerTwoLdLoss;
 var ldFinalThree = ldPayoutThree - playerThreeLdLoss;
 var ldFinalFour = ldPayoutFour - playerFourLdLoss;
 //
-// console.log(ldFinalOne);
-// console.log(ldFinalTwo);
-// console.log(ldFinalThree);
-// console.log(ldFinalFour);
+console.log("LD Final: " + ldFinalOne);
+console.log("LD Final: " + ldFinalTwo);
+console.log("LD Final: " + ldFinalThree);
+console.log("LD Final: " + ldFinalFour);
 
 var playerOneLosses = (parseInt(totalHolesWon) - parseInt(playerOne.totalWins)) * holeBet;
 var playerTwoLosses = (parseInt(totalHolesWon) - parseInt(playerTwo.totalWins)) * holeBet;
 var playerThreeLosses = (parseInt(totalHolesWon) - parseInt(playerThree.totalWins)) * holeBet;
-var playerFourLosses = (parseInt(totalHolesWon) - parseInt(playerFour.totalWins)) * holeBet;
-// console.log(parseInt(playerOneLosses) * holeBet + " lost P1");
-// console.log(parseInt(playerTwoLosses) * holeBet + " lost P2");
-// console.log(parseInt(playerThreeLosses) * holeBet + " lost P3");
-// console.log(parseInt(playerFourLosses) * holeBet + " lost P4");
+var playerFourLosses = (parseInt(totalHolesWon) - parseInt(playerFour.totalWins)) * holeBet || 0;
+console.log(parseInt(playerOneLosses) * holeBet + " lost P1 Hole bets");
+console.log(parseInt(playerTwoLosses) * holeBet + " lost P2");
+console.log(parseInt(playerThreeLosses) * holeBet + " lost P3");
+console.log(parseInt(playerFourLosses) * holeBet + " lost P4");
 
-var playerOneWins = (parseInt(playerOne.totalWins) * parseInt(holeBet)) * 3;
-var playerTwoWins = (parseInt(playerTwo.totalWins) * parseInt(holeBet)) * 3;
-var playerThreeWins = (parseInt(playerThree.totalWins) * parseInt(holeBet)) * 3;
-var playerFourWins = (parseInt(playerFour.totalWins) * parseInt(holeBet)) * 3;
-// console.log(playerOneWins + " P1won");
-// console.log(playerTwoWins + " P2won");
-// console.log(playerThreeWins + " P3won");
-// console.log(playerFourWins + " P4won");
+var playerOneWins = (parseInt(playerOne.totalWins) * parseInt(holeBet)) * parseInt(playerNumber - 1);
+var playerTwoWins = (parseInt(playerTwo.totalWins) * parseInt(holeBet)) * parseInt(playerNumber - 1);
+var playerThreeWins = (parseInt(playerThree.totalWins) * parseInt(holeBet)) * parseInt(playerNumber - 1);
+var playerFourWins = (parseInt(playerFour.totalWins) * parseInt(holeBet)) * parseInt(playerNumber - 1) || 0;
+console.log(playerOneWins + " P1won");
+console.log(playerTwoWins + " P2won");
+console.log(playerThreeWins + " P3won");
+console.log(playerFourWins + " P4won");
 
 var playerOneKpTakehome = (parseInt(playerOne.kps) - parseInt(totalKps)) * kpBet;
 var playerTwoKpTakehome = (parseInt(playerTwo.kps) - parseInt(totalKps)) * kpBet;
 var playerThreeKpTakehome = (parseInt(playerThree.kps) - parseInt(totalKps)) * kpBet;
 var playerFourKpTakehome = (parseInt(playerFour.kps) - parseInt(totalKps)) * kpBet;
-// console.log(playerOneKpTakehome + " kp take home");
-// console.log(playerTwoKpTakehome + " kp take home");
-// console.log(playerThreeKpTakehome + " kp take home");
-// console.log(playerFourKpTakehome + " kp take home");
+console.log(playerOneKpTakehome + " kp take home");
+console.log(playerTwoKpTakehome + " kp take home");
+console.log(playerThreeKpTakehome + " kp take home");
+console.log(playerFourKpTakehome + " kp take home");
 
 
 var holesTotalOne = playerOneWins - playerOneLosses;
 var holesTotalTwo = playerTwoWins - playerTwoLosses;
 var holesTotalThree = playerThreeWins - playerThreeLosses;
-var holesTotalFour = playerFourWins - playerFourLosses;
-// console.log(holesTotalOne);
-// console.log(holesTotalTwo);
+var holesTotalFour = playerFourWins - playerFourLosses || 0;
+console.log("holes total: " + holesTotalOne);
+console.log("holes total: " + holesTotalTwo);
 
 
 
@@ -178,12 +180,12 @@ document.getElementById('playerFourHolesTotal').innerHTML = holesTotalFour + "$"
 document.getElementById('playerOneKpPayout').innerHTML = playerOneKpTotal + "$";
 document.getElementById('playerTwoKpPayout').innerHTML = playerTwoKpTotal + "$";
 document.getElementById('playerThreeKpPayout').innerHTML = playerThreeKpTotal + "$";
-document.getElementById('playerFourKpPayout').innerHTML = playerFourKpTotal + "$";
+document.getElementById('playerFourKpPayout').innerHTML = playerFourKpTotal + "$" ;
 
 document.getElementById('playerOneLdFinal').innerHTML = ldFinalOne + "$";
 document.getElementById('playerTwoLdFinal').innerHTML = ldFinalTwo + "$";
 document.getElementById('playerThreeLdFinal').innerHTML = ldFinalThree + "$";
-document.getElementById('playerFourLdFinal').innerHTML = ldFinalFour + "$";
+document.getElementById('playerFourLdFinal').innerHTML = ldFinalFour + "$" ;
 
 document.getElementById('playerOneSplits').innerHTML = splitOneTot + "$";
 document.getElementById('playerTwoSplits').innerHTML = splitTwoTot + "$";
@@ -191,29 +193,41 @@ document.getElementById('playerThreeSplits').innerHTML = splitThreeTot + "$";
 document.getElementById('playerFourSplits').innerHTML = splitFourTot + "$";
 
 document.getElementById('playerOneCash').innerHTML = parseInt(holesTotalOne + playerOneKpTotal
-                                                    + ldFinalOne + splitOneTot) + "$ net total!"
+                                                    + ldFinalOne + splitOneTot ) + "$ net total!";
 document.getElementById('playerTwoCash').innerHTML = parseInt(holesTotalTwo + playerTwoKpTotal
-                                                    + ldFinalTwo + splitTwoTot) + "$ net total!"
+                                                    + ldFinalTwo + splitTwoTot ) + "$ net total!";
 document.getElementById('playerThreeCash').innerHTML = parseInt(holesTotalThree + playerThreeKpTotal
-                                                    + ldFinalThree + splitThreeTot) + "$ net total!"
+                                                    + ldFinalThree + splitThreeTot ) + "$ net total!";
 document.getElementById('playerFourCash').innerHTML = parseInt(holesTotalFour + playerFourKpTotal
-                                                    + ldFinalFour + splitFourTot) + "$ net total!"
+                                                    + ldFinalFour + splitFourTot) + "$ net total!" ;
 
-document.getElementById('playerOneName').innerHTML = playerOne.name.toUpperCase();
-document.getElementById('playerTwoName').innerHTML = playerTwo.name.toUpperCase();
-document.getElementById('playerThreeName').innerHTML = playerThree.name.toUpperCase();
-document.getElementById('playerFourName').innerHTML = playerFour.name.toUpperCase();
+document.getElementById('playerOneName').innerHTML = playerOne.name;
+document.getElementById('playerTwoName').innerHTML = playerTwo.name;
+document.getElementById('playerThreeName').innerHTML = playerThree.name;
+document.getElementById('playerFourName').innerHTML = playerFour.name ;
 
+ if(playerNumber < 4){
+   document.getElementById('playerFourCash').innerHTML = "";
+   document.getElementById('playerFourLdFinal').innerHTML = "";
+   document.getElementById('playerFourKpPayout').innerHTML = "";
+   document.getElementById('playerFourHolesTotal').innerHTML = "";
+   
+   document.getElementById('playerOneSplits').innerHTML = "";
+   document.getElementById('playerTwoSplits').innerHTML = "";
+   document.getElementById('playerThreeSplits').innerHTML = "";
+   document.getElementById('playerFourSplits').innerHTML = "";
+ }
 
 
 
 }
 
 function save(){
+
   playerOne.name = document.getElementById('playerOne').value;
   playerTwo.name = document.getElementById('playerTwo').value;
   playerThree.name = document.getElementById('playerThree').value;
-  playerFour.name = document.getElementById('playerFour').value;
+  playerFour.name = document.getElementById('playerFour').value ;
 
 
 
@@ -239,33 +253,46 @@ document.getElementById('fourSplitsName').innerHTML = " " + playerFour.name;
 toggleDisabled();
 
 
+
 }
 
+var playerNumber = 0;
 function toggleDisabled(poop){
+
       if(!poop && document.getElementById('playerOne').name.length > 0) {
+        playerNumber = playerNumber + 1;
         document.getElementById('oneWins').removeAttribute("disabled");
         document.getElementById('oneLds').removeAttribute("disabled");
         document.getElementById('oneKps').removeAttribute("disabled")
-        document.getElementById('splitsOne').removeAttribute("disabled")
+        //
       } if (!poop && document.getElementById('playerTwo').name.length > 0) {
+        playerNumber = playerNumber + 1;
         document.getElementById('twoWins').removeAttribute("disabled");
         document.getElementById('twoLds').removeAttribute("disabled");
         document.getElementById('twoKps').removeAttribute("disabled")
-        document.getElementById('splitsTwo').removeAttribute("disabled")
+        //
       } if (!poop && document.getElementById('playerThree').name.length > 0) {
+        playerNumber = playerNumber + 1;
         document.getElementById('threeWins').removeAttribute("disabled");
         document.getElementById('threeLds').removeAttribute("disabled");
         document.getElementById('threeKps').removeAttribute("disabled")
-        document.getElementById('splitsThree').removeAttribute("disabled")
+      //
       } if (!poop && document.getElementById('playerFour').name.length > 0) {
+        playerNumber = playerNumber + 1;
         document.getElementById('fourWins').removeAttribute("disabled");
         document.getElementById('fourLds').removeAttribute("disabled");
         document.getElementById('fourKps').removeAttribute("disabled")
-        document.getElementById('splitsFour').removeAttribute("disabled")
-      }
+
+    } if (playerNumber > 3) {
+      document.getElementById('splitsOne').removeAttribute("disabled")
+      document.getElementById('splitsTwo').removeAttribute("disabled")
+      document.getElementById('splitsThree').removeAttribute("disabled")
+      document.getElementById('splitsFour').removeAttribute("disabled")
+    }
+
+      return playerNumber;
+      console.log("playerNumber: " + playerNumber);
   }
-
-
 
 
 
